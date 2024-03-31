97870b349efa2c33a698c45791a1dcac5d8a4c1e
Eduardo Afonso <eduardo_asj@hotmail.com>
2024-03-31 00:28:33 -0300

    Adding titles and event

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,30 @@
-import '../styles/header.scss'
+import React from 'react';
+import '../styles/header.scss';
 
 export function Header() {
+  const handleClick = () => {
+    console.log('Header clicked!');
+  };
+
   return (
-    <header className="header">
-      <div>
-        <img src="/logo.svg" alt="to.do"/>
+    <header className='header' onClick={handleClick}>
+      <div className='logo-container'>
+        <img src='/logo.svg' alt='to.do' className='logo' />
+        <h1 className='title'>To-Do App</h1>
       </div>
+      <nav className='navigation'>
+        <ul className='nav-list'>
+          <li className='nav-item'>
+            <a href='#'>Home</a>
+          </li>
+          <li className='nav-item'>
+            <a href='#'>About</a>
+          </li>
+          <li className='nav-item'>
+            <a href='#'>Contact</a>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
